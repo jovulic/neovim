7536f8ea09cd9bc05ef6bef914e9045079eab519
Josip Vulic <jovulic@gmail.com>
2025-03-27 13:31:39 -0400

    del(user): disable lsp signature hints

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -13,7 +13,11 @@ return {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
+    config = function()
+      require("lsp_signature").setup {
+        hint_enable = false,
+      }
+    end,
   },
 
   -- == Examples of Overriding Plugins ==
