cfd30daaeaa93ac2c03e50fd9a2f659ce51caa98
Josip Vulic <jovulic@gmail.com>
2025-10-11 13:22:42 -0400

    fix(treesitter): update treesitter with correct branch references

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,13 +4,20 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      -- "lua",
-      -- "vim",
-      -- add more arguments for adding more treesitter parsers
-      "authzed",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    opts = {
+      ensure_installed = {
+        -- "lua",
+        -- "vim",
+        -- add more arguments for adding more treesitter parsers
+        "authzed",
+      },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
   },
 }
