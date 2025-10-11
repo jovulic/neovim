028cbffae2f399d2fac6a3d4a8ac73cf92d5454a
Josip Vulic <jovulic@gmail.com>
2025-10-11 13:21:34 -0400

    upd(code-companion): use new option format

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -82,147 +82,149 @@ return {
     },
     opts = {
       adapters = {
-        gemini = function()
-          return require("codecompanion.adapters").extend("gemini", {
-            env = {
-              api_key = "cmd:gopass show -no google.com/jovulic@gmail.com/ai-api-key",
-            },
-            schema = {
-              model = {
-                default = "gemini-2.5-flash-preview-05-20",
-                choices = {
-                  "gemini-2.5-pro-exp-03-25",
-                  "gemini-2.5-flash-preview-05-20",
+        http = {
+          gemini = function()
+            return require("codecompanion.adapters").extend("gemini", {
+              env = {
+                api_key = "cmd:gopass show -no google.com/jovulic@gmail.com/ai-api-key",
+              },
+              schema = {
+                model = {
+                  default = "gemini-2.5-flash-preview-05-20",
+                  choices = {
+                    "gemini-2.5-pro-exp-03-25",
+                    "gemini-2.5-flash-preview-05-20",
+                  },
                 },
               },
-            },
-          })
-        end,
-        gpt_oss = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            schema = {
-              name = "gps_oss",
-              formatted_name = "GPT OSS",
-              model = {
-                default = "gpt-oss:20b",
-                choices = {
-                  "gpt-oss:20b",
+            })
+          end,
+          gpt_oss = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                name = "gps_oss",
+                formatted_name = "GPT OSS",
+                model = {
+                  default = "gpt-oss:20b",
+                  choices = {
+                    "gpt-oss:20b",
+                  },
                 },
               },
-            },
-          })
-        end,
-        codestral = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            schema = {
-              name = "codestral",
-              formatted_name = "Codestral",
-              model = {
-                default = "codestral",
-                choices = {
-                  "codestral",
+            })
+          end,
+          codestral = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                name = "codestral",
+                formatted_name = "Codestral",
+                model = {
+                  default = "codestral",
+                  choices = {
+                    "codestral",
+                  },
                 },
               },
-            },
-          })
-        end,
-        gemma = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            schema = {
-              name = "gemma",
-              formatted_name = "Gemma",
-              model = {
-                default = "gemma3:12b",
-                choices = {
-                  "gemma3",
-                  "gemma3:12b",
+            })
+          end,
+          gemma = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                name = "gemma",
+                formatted_name = "Gemma",
+                model = {
+                  default = "gemma3:12b",
+                  choices = {
+                    "gemma3",
+                    "gemma3:12b",
+                  },
                 },
               },
-            },
-          })
-        end,
-        deepseek_coder = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            schema = {
-              name = "deepseek-coder",
-              formatted_name = "DeepSeek Coder",
-              model = {
-                default = "deepseek-coder-v2",
-                choices = {
-                  "deepseek-coder-v2",
+            })
+          end,
+          deepseek_coder = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                name = "deepseek-coder",
+                formatted_name = "DeepSeek Coder",
+                model = {
+                  default = "deepseek-coder-v2",
+                  choices = {
+                    "deepseek-coder-v2",
+                  },
                 },
               },
-            },
-          })
-        end,
-        deepseek = function()
-          local state = ""
-          local state_count = 0
-          return require("codecompanion.adapters").extend("ollama", {
-            name = "deepseek-r1",
-            formatted_name = "DeepSeek r1",
-            schema = {
-              model = {
-                default = "deepseek-r1:14b",
-                choices = {
-                  ["deepseek-r1:14b"] = { opts = { can_reason = true } },
+            })
+          end,
+          deepseek = function()
+            local state = ""
+            local state_count = 0
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "deepseek-r1",
+              formatted_name = "DeepSeek r1",
+              schema = {
+                model = {
+                  default = "deepseek-r1:14b",
+                  choices = {
+                    ["deepseek-r1:14b"] = { opts = { can_reason = true } },
+                  },
                 },
               },
-            },
-            handlers = {
-              chat_output = function(_, data)
-                local output = {}
+              handlers = {
+                chat_output = function(_, data)
+                  local output = {}
 
-                if data and data ~= "" then
-                  local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-                  if not ok then return { status = "error" } end
+                  if data and data ~= "" then
+                    local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
+                    if not ok then return { status = "error" } end
 
-                  local message = json.message
-                  if message.content then
-                    local content = message.content
+                    local message = json.message
+                    if message.content then
+                      local content = message.content
 
-                    output.role = message.role or nil
+                      output.role = message.role or nil
 
-                    -- We want to also ignore the newlines that follow the state
-                    -- transitions.
-                    if state == "REASON" and state_count == 1 then content = "" end
-                    if state == "RESPONSE" and state_count == 1 then content = "" end
+                      -- We want to also ignore the newlines that follow the state
+                      -- transitions.
+                      if state == "REASON" and state_count == 1 then content = "" end
+                      if state == "RESPONSE" and state_count == 1 then content = "" end
 
-                    -- Look for <think> tag and switch to state "REASON".
-                    -- We also clear the content here to ignore the tag.
-                    if content == "<think>" then
-                      state = "REASON"
-                      state_count = 0
-                      content = ""
+                      -- Look for <think> tag and switch to state "REASON".
+                      -- We also clear the content here to ignore the tag.
+                      if content == "<think>" then
+                        state = "REASON"
+                        state_count = 0
+                        content = ""
+                      end
+
+                      -- Look for </think> tag and switch to state "RESPONSE".
+                      -- We also clear the content here to ignore the tag.
+                      if content == "</think>" then
+                        state = "RESPONSE"
+                        state_count = 0
+                        content = ""
+                      end
+
+                      -- Set the reasoning or content output properties accordingly.
+                      if state == "REASON" then
+                        output.reasoning = content
+                      else
+                        output.content = content
+                      end
                     end
 
-                    -- Look for </think> tag and switch to state "RESPONSE".
-                    -- We also clear the content here to ignore the tag.
-                    if content == "</think>" then
-                      state = "RESPONSE"
-                      state_count = 0
-                      content = ""
-                    end
+                    state_count = state_count + 1
 
-                    -- Set the reasoning or content output properties accordingly.
-                    if state == "REASON" then
-                      output.reasoning = content
-                    else
-                      output.content = content
-                    end
+                    return {
+                      status = "success",
+                      output = output,
+                    }
                   end
-
-                  state_count = state_count + 1
-
-                  return {
-                    status = "success",
-                    output = output,
-                  }
-                end
-              end,
-            },
-          })
-        end,
+                end,
+              },
+            })
+          end,
+        },
       },
       strategies = {
         chat = { adapter = "gpt_oss" },
