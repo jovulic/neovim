8fcad33f49fde29af3dfbc206de632a5f09da9d0
Josip Vulic <jovulic@gmail.com>
2025-10-19 18:26:23 -0400

    upd(astrolsp): remove volar lsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -31,7 +31,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
-        "volar",
+        -- "volar",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -50,7 +50,7 @@ return {
       "nixd",
       "gopls",
       "vtsls",
-      "volar",
+      "vue_ls",
       "basedpyright",
       "clangd",
     },
